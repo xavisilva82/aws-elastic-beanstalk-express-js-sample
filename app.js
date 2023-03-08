b737e1352f816453f9b6666077523023156a60d1
xavisilva <xavisilva.dev@gmail.com>
2023-03-07 20:22:44 -0600

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hola Xavier'));
+app.get('/', (req, res) => res.send('Hola Xavier-Lab'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
