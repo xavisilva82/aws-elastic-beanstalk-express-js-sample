67bc3e19cfa9310d4fa2b3880baf73ce0736e07d
XaviSilva <109827705+xavisilva82@users.noreply.github.com>
2025-06-28 08:32:35 -0500

    Add Flutter tracking app

---
+++ driver_tracking_flutter/lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'login_page.dart';
+import 'tracking_page.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Driver Tracking',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.active) {
+            final user = snapshot.data;
+            if (user == null) {
+              return const LoginPage();
+            } else {
+              return TrackingPage(user: user);
+            }
+          }
+          return const Scaffold(body: Center(child: CircularProgressIndicator()));
+        },
+      ),
+    );
+  }
+}
